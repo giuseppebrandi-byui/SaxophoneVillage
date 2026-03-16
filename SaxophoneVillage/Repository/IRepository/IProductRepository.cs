f417a98f5b8a433b0d45a46cb9e4a05faea426a0
Giuseppe Elia Brandi <bra22041@byui.edu>
2026-03-16 19:36:37 +0000

    Add Product Repository and Components

---
+++ SaxophoneVillage/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using SaxophoneVillage.Data;
+
+namespace SaxophoneVillage.Repository.IRepository
+{
+    public interface IProductRepository
+    {
+        public Task<Product> CreateAsync(Product obj);
+        public Task<Product> UpdateAsync(Product obj);
+        public Task<bool> DeleteAsync(int id);
+        public Task<Product> GetAsync(int id);
+        public Task<IEnumerable<Product>> GetAllAsync();
+
+    }
+}
